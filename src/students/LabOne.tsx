4a5c8258c7ff1e0a32e6ad56d72b4f1251975d16
John Paxton <johnpaxton@gmail.com>
2022-09-19 18:45:16 -0400

    Lab 1: solution

--- src/students/LabOne.tsx
+++ src/students/LabOne.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Student } from '../types';
+
+const getAllStudents = gql`
+  query GetAllStudents {
+    allStudents {
+      firstName
+      lastName
+      city
+      province
+      country
+    }
+  }
+`;
 
 const LabOne = () => {
-  return <p>The first lab. Good luck!</p>;
+  const { loading, error, data } = useQuery(getAllStudents);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong: {error.message}</p>;
+
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>City</th>
+          <th>Province</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.allStudents.map((student: Student) => {
+          return (
+            <tr>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.city}</td>
+              <td>{student.province}</td>
+              <td>{student.country}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default LabOne;
