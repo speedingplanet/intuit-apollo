cd99d99533488ed2a633d5368f905d50ad0e1d16
John Paxton <johnpaxton@gmail.com>
2022-09-20 17:54:08 -0400

    Departments and another query

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // Server implementation here
 const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageLocalDefault } = require('apollo-server-core');
-const { rooms } = require('../data/all-data.cjs');
+const { rooms, departments } = require('../data/all-data.cjs');
 
 // We need data, graphql type definitions, resolvers for our data
 
@@ -13,8 +13,15 @@ const typeDefs = gql`
     roomCapacity: Int
   }
 
+  type Department {
+    id: ID!
+    departmentName: String!
+  }
+
   type Query {
     rooms: [Room]
+    roomById(id: ID!): Room
+    departments: [Department]
   }
 `;
 
@@ -22,13 +29,14 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     rooms: () => rooms, // lots of rooms, one would think
+    roomById: (parent, { id }) => rooms.find((room) => room.id === Number(id)),
+    departments: () => departments,
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  csrfPrevention: true, // Patch a security hole
   cache: 'bounded', // Limited cache, about 30 MB, can configure for me
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })], // Apollo's version of GraphiQL
 });
